85d69cbb0cf70a3d1db3c40f2e8b3bd10f9873eb
Magons <vano47@bk.ru>
2017-12-14 07:49:52 +0200

    Install gems; Install devise

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create(
+  [
+    {
+      email: 'pierre@michaux.com',
+      name: 'Pierre',
+      password: 'progenitor',
+      password_confiramtion: 'progenitor'
+    },
+    {
+      email: 'test@test.com',
+      name: 'Ivan',
+      password: 'progenitor',
+      password_confiramtion: 'progenitor'
+    }
+  ]
+)
